08dd78b2b2221f6f022fd1402330a1e893f4b3a8
Pichau <gustavosandrindattein123@gmail.com>
2022-06-15 14:16:52 -0300

    testes com exceções

--- libpythonproo/tests/test_spam/test_enviador_de_email.py
+++ libpythonproo/tests/test_spam/test_enviador_de_email.py
@@ -1,6 +1,6 @@
 import pytest
 
-from libpythonproo.spam.enviador_de_email import Enviador
+from libpythonproo.spam.enviador_de_email import Enviador, EmailInvalido
 
 
 def test_criar_enviador_de_email():
@@ -19,5 +19,20 @@ def test_remetente(remetente):
         'gustavosandrindattein321@gmail.com',
         'Cursos Python Pro',
         'Primeira turma do curso'
-     )
+    )
     assert remetente in resultado
+
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['', 'aleatoriateste']
+)
+def test_remetente_invalido(remetente):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        enviador.enviar(
+            remetente,
+            'gustavosandrindattein321@gmail.com',
+            'Cursos Python Pro',
+            'Primeira turma do curso'
+        )
---
+++ libpythonproo/tests/test_spam/test_usuario.py
@@ -0,0 +1,3 @@
+def test_salvar_usuario():
+    conexao = Conexao()
+    s

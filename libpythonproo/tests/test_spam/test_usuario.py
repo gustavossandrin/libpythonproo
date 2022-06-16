038de9194e40fc06cd515e5f9116d159889276aa
Pichau <gustavosandrindattein123@gmail.com>
2022-06-16 10:39:53 -0300

    setup e tear down com fixture

--- libpythonproo/tests/test_spam/test_usuario.py
+++ libpythonproo/tests/test_spam/test_usuario.py
@@ -1,25 +1,36 @@
+import pytest
+
 from libpythonproo.spam.db import Conexao
 from libpythonproo.spam.modelos import Usuario
 
 
-def test_salvar_usuario():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
+@pytest.fixture()
+def conexao():
+    # setup
+    conexao_obj = Conexao()
+    yield conexao_obj
+    # tear down
+    conexao_obj.fechar()
+
+
+@pytest.fixture()
+def sessao(conexao):
+    sessao_obj = conexao.gerar_sessao()
+    yield sessao_obj
+    sessao_obj.roll_back()
+    sessao_obj.fechar()
+
+
+def test_salvar_usuario(sessao):
     usuario = Usuario(nome='gustavo')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()
 
 
-def test_listar_usuarios():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
+
+
+def test_listar_usuarios(sessao):
     usuarios = [Usuario(nome='gustavo'), Usuario(nome='guilherme')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()

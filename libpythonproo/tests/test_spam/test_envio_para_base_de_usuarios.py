5b1c577c9945bce9ef304cd2039f44dc6df54842
Pichau <gustavosandrindattein123@gmail.com>
2022-06-16 20:19:39 -0300

    mock

--- libpythonproo/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonproo/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,19 +1,9 @@
+from unittest.mock import Mock
+
 import pytest
 
-from libpythonproo.spam.enviador_de_email import Enviador
 from libpythonproo.spam.main import EnviadorDeSpam
 from libpythonproo.spam.modelos import Usuario
-
-
-class EnviadorMock(Enviador):
-    def __init__(self):
-        super().__init__()
-        self.qtd_email_enviados = 0
-        self.parametros_de_envio = None
-
-    def enviar(self, remetente, destinatario, assunto, corpo):
-        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
-        self.qtd_email_enviados += 1
 
 
 @pytest.mark.parametrize(
@@ -31,29 +21,29 @@ class EnviadorMock(Enviador):
 def test_qde_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'gustavossandrindattein123@gmail.com',
         'curso python pro',
         'confira os módulos fantásticos'
     )
-    assert len(usuarios) == enviador.qtd_email_enviados
+    assert len(usuarios) == enviador.enviar.call_count
 
 
 def test_parametros_de_spam(sessao):
     usuario = Usuario(nome='gustavo', email='gustavosandrindattein123@gmail.com')
     sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'gustavo123@gmail.com',
         'curso python pro',
         'confira os módulos fantásticos'
     )
-    assert enviador.parametros_de_envio == (
+    enviador.enviar.assert_called_once_with == (
         'gustavo123@gmail.com',
         'gustavosandrindattein123@gmail.com',
         'curso python pro',
         'confira os módulos fantásticos'
-    )
+     )
